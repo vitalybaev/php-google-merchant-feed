7645c7acfeaecff7fca4f2c2d7905ecd4e55bec3
Alexandr Krylov <krylov123@gmail.com>
2017-11-22 18:23:24 +0300

    Fix Availability values to meet latest google merchant requirements

--- src/Product/Availability/Availability.php
+++ src/Product/Availability/Availability.php
@@ -4,9 +4,9 @@ namespace Vitalybaev\GoogleMerchant\Product\Availability;
 
 class Availability
 {
-    const IN_STOCK = 'in_stock';
+    const IN_STOCK = 'in stock';
 
-    const OUT_OF_STOCK = 'out_of_stock';
+    const OUT_OF_STOCK = 'out of stock';
 
     const PREORDER = 'preorder';
 }
