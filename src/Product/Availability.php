988cea5e3b7e45631bc4c3cf337c43372fd94283
JS Morisset <jsm@surniaulula.com>
2023-11-25 14:12:59 -0800

    added setFacebookAvailability()

---
+++ src/Product/Availability.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vitalybaev\GoogleMerchant\Product;
+
+class Availability
+{
+    const IN_STOCK = 'in_stock';
+
+    const OUT_OF_STOCK = 'out_of_stock';
+
+    const PREORDER = 'preorder';
+    
+    const BACKORDER = 'backorder';
+}
+
+class FacebookAvailability
+{
+    const IN_STOCK = 'in stock';
+
+    const OUT_OF_STOCK = 'out of stock';
+}
