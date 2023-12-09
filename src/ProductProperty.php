a31305fd5087b1612ca4da7ac87093cfbddc2b16
JS Morisset <jsm@surniaulula.com>
2023-12-09 10:07:27 -0800

    converted spaces to tabs

--- src/ProductProperty.php
+++ src/ProductProperty.php
@@ -6,102 +6,102 @@ use Sabre\Xml\Element\Cdata;
 
 class ProductProperty
 {
-    /**
-     * @var string
-     */
-    private $name;
+	/**
+	 * @var string
+	 */
+	private $name;
 
-    /**
-     * @var string
-     */
-    private $value;
+	/**
+	 * @var string
+	 */
+	private $value;
 
-    /**
-     * @var bool
-     */
-    private $isCData = false;
+	/**
+	 * @var bool
+	 */
+	private $isCData = false;
 
-    /**
-     * ProductProperty constructor.
-     *
-     * @param      $name
-     * @param      $value
-     * @param bool $isCData
-     */
-    public function __construct($name, $value, $isCData)
-    {
-        $this->name = strtolower($name);
-        $this->value = $value;
-        $this->isCData = $isCData;
-    }
+	/**
+	 * ProductProperty constructor.
+	 *
+	 * @param      $name
+	 * @param      $value
+	 * @param bool $isCData
+	 */
+	public function __construct( $name, $value, $isCData )
+	{
+		$this->name    = strtolower( $name );
+		$this->value   = $value;
+		$this->isCData = $isCData;
+	}
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
 
-    /**
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+	/**
+	 * @param string $name
+	 */
+	public function setName( $name )
+	{
+		$this->name = $name;
+	}
 
-    /**
-     * @return string|PropertyBag
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
+	/**
+	 * @return string|PropertyBag
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
 
-    /**
-     * @param string $value
-     */
-    public function setValue($value)
-    {
-        $this->value = $value;
-    }
+	/**
+	 * @param string $value
+	 */
+	public function setValue( $value )
+	{
+		$this->value = $value;
+	}
 
-    /**
-     * @return bool
-     */
-    public function isCData()
-    {
-        return $this->isCData;
-    }
+	/**
+	 * @return bool
+	 */
+	public function isCData()
+	{
+		return $this->isCData;
+	}
 
-    /**
-     * @param bool $isCData
-     */
-    public function setIsCData($isCData)
-    {
-        $this->isCData = $isCData;
-    }
+	/**
+	 * @param bool $isCData
+	 */
+	public function setIsCData( $isCData )
+	{
+		$this->isCData = $isCData;
+	}
 
-    /**
-     * @param $namespace
-     *
-     * @return array
-     */
-    public function getXmlStructure($namespace)
-    {
-        $value = $this->isCData() ? new Cdata($this->getValue()) : $this->getValue();
-        
-        if (is_object($value) && $value instanceof PropertyBag) {
-            return [
-                'name' => $namespace . $this->getName(),
-                'value' => $value->getPropertiesXmlStructure($namespace),
-            ];
-        }
+	/**
+	 * @param $namespace
+	 *
+	 * @return array
+	 */
+	public function getXmlStructure( $namespace )
+	{
+		$value = $this->isCData() ? new Cdata( $this->getValue() ) : $this->getValue();
+		
+		if ( is_object( $value ) && $value instanceof PropertyBag ) {
+			return [
+				'name'  => $namespace . $this->getName(),
+				'value' => $value->getPropertiesXmlStructure( $namespace ),
+			];
+		}
 
-        return [
-            'name' => $namespace . $this->getName(),
-            'value' => $value,
-        ];
-    }
+		return [
+			'name'  => $namespace . $this->getName(),
+			'value' => $value,
+		];
+	}
 }
